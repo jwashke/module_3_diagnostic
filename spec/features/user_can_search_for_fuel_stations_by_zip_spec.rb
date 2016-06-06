87a31bf00c24a1c272d29333352a7d6edc83b79f
Josh Washke <jwashke@icloud.com>
2016-06-06 10:16:28 -0600

    Create model test for station

--- spec/features/user_can_search_for_fuel_stations_by_zip_spec.rb
+++ spec/features/user_can_search_for_fuel_stations_by_zip_spec.rb
@@ -18,57 +18,12 @@ RSpec.feature "User can search for fuel stations by zip code" do
         click_button "Locate"
 
         within(".station-66897") do
-          expect(page).to have_content?("UDR")
-          expect(page).to have_content?("800 Acoma St Denver CO 80204")
-          expect(page).to have_content?("0.31 miles")
-          expect(page).to have_content?("Access Times: 24 hours daily")
+          expect(page).to have_content("UDR")
+          expect(page).to have_content("800 Acoma St Denver CO 80204")
+          expect(page).to have_content("0.3117 miles")
+          expect(page).to have_content("Access Times: 24 hours daily")
         end
       end
     end
   end
 end
-
-# "access_days_time": "24 hours daily",
-#   "cards_accepted": null,
-#   "date_last_confirmed": "2016-06-06",
-#   "expected_date": null,
-#   "fuel_type_code": "ELEC",
-#   "id": 66897,
-  # "groups_with_access_code": "Public",
-  # "open_date": null,
-  # "owner_type_code": null,
-  # "status_code": "E",
-  # "station_name": "UDR",
-  # "station_phone": "888-758-4389",
-  # "updated_at": "2016-06-06T08:06:43Z",
-  # "geocode_status": "GPS",
-  # "latitude": 39.7300224,
-  # "longitude": -104.9883424,
-  # "city": "Denver",
-  # "intersection_directions": "816 ACOMA 1; For residents only - 5th Floor",
-  # "plus4": null,
-  # "state": "CO",
-  # "street_address": "800 Acoma St",
-  # "zip": "80204",
-  # "bd_blends": null,
-  # "e85_blender_pump": null,
-  # "ev_connector_types": [
-  #   "J1772"
-  # ],
-  # "ev_dc_fast_num": null,
-  # "ev_level1_evse_num": null,
-  # "ev_level2_evse_num": 2,
-  # "ev_network": "ChargePoint Network",
-  # "ev_network_web": "http://www.mychargepoint.net/",
-  # "ev_other_evse": null,
-  # "hy_status_link": null,
-  # "lpg_primary": null,
-  # "ng_fill_type_code": null,
-  # "ng_psi": null,
-  # "ng_vehicle_class": null,
-  # "ev_network_ids": {
-  #   "posts": [
-  #     "1:118249"
-  #   ]
-  # },
-  # "distance": 0.3117
--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Station, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".get_stations" do
+    it "returns an array of 10 station objects" do
+      VCR.use_cassette "Station.get_stations model test" do
+        stations = Station.get_stations("80203")
+
+        expect(stations.count).to eq(10)
+        expect(stations.first.class).to eq(Station)
+      end
+    end
+  end
 end
