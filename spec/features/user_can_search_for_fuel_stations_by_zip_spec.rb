02032147785633d89c0daa257bb64a8b33cca06e
Josh Washke <jwashke@icloud.com>
2016-06-06 10:26:44 -0600

    Create Second feature test, modify search in navbar
    Create second feature test for search with given search radius in miles
    Modify search form in navbar to add text field for radius

--- spec/features/user_can_search_for_fuel_stations_by_zip_spec.rb
+++ spec/features/user_can_search_for_fuel_stations_by_zip_spec.rb
@@ -17,12 +17,40 @@ RSpec.feature "User can search for fuel stations by zip code" do
         fill_in :q, with: "80203"
         click_button "Locate"
 
-        within(".station-66897") do
+        expect(page).to have_selector ".station-name", count: 10
+        within("#station-66897") do
           expect(page).to have_content("UDR")
           expect(page).to have_content("800 Acoma St Denver CO 80204")
           expect(page).to have_content("0.3117 miles")
           expect(page).to have_content("Access Times: 24 hours daily")
         end
+      end
+    end
+  end
+
+  context "with a given distance" do
+    it "displays the 10 closest stations within given distance with pagination links for rest" do
+      VCR.use_cassette "5 mile search by zip" do
+        visit root_path
+
+        within(".search") do
+          fill_in :q, with: "80203"
+          fill_in :r, with: "5"
+          click_button "Locate"
+        end
+
+        expect(page).to have_selector ".station-name", count: 10
+        within("#station-66897") do
+          expect(page).to have_content("UDR")
+          expect(page).to have_content("800 Acoma St Denver CO 80204")
+          expect(page).to have_content("0.3117 miles")
+          expect(page).to have_content("Access Times: 24 hours daily")
+        end
+        expect(page).to have_link("2")
+        expect(page).to have_link("3")
+        expect(page).to have_link("4")
+        expect(page).to have_link("5")
+        expect(page).to have_link("6")
       end
     end
   end
